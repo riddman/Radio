3a965824a471c4ee4b5d58ba3dc8dae0937c80c5
riddman <riddleman@ukr.net>
2021-01-03 23:30:45 +0200

    Displayed dumb data

--- lib/RadioList.dart
+++ lib/RadioList.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:radio/RadioListBag.dart';
 
 class RadioList extends StatelessWidget {
+    List<RadioListBag> data = [
+      RadioListBag(name: 'Хіт FM', title: 'Хіт FM', description: 'Тільки хіти! Стилі: dance, pop, disco, pop-rock, rock, rnb. Сайт: https://www.hitfm.ua'),
+      RadioListBag(name: 'Люкс ФМ', title: 'Люкс ФМ', description: 'Радіо виключно гарного настрою. Веселі ведучі, жарти, добірна сучасна музика. Ви любите жити, любите вечірки з друзями і не любите спати? Тоді настроюйся на хвилю радіо Люкс ФМ.')
+    ];
+
     @override
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
           title: Text('App bar title')
         ),
-        body: Text('Body Body Body')
+        //body: Text('Body Body Body')
+        body: Container(
+          child: ListView(
+              children: <Widget>[
+                ListTile(title: Text('Хіт FM'), subtitle: Text('Тільки хіти! Стилі: dance, pop, disco, pop-rock, rock, rnb. Сайт: https://www.hitfm.ua')),
+                ListTile(title: Text('Люкс ФМ'), subtitle: Text('Радіо виключно гарного настрою. Веселі ведучі, жарти, добірна сучасна музика. Ви любите жити, любите вечірки з друзями і не любите спати? Тоді настроюйся на хвилю радіо Люкс ФМ.'))
+              ]
+          )
+        )
       );
     }
 }
---
+++ lib/RadioListBag.dart
@@ -0,0 +1,7 @@
+class RadioListBag {
+  String name;
+  String title;
+  String description;
+
+  RadioListBag({this.name, this.title, this.description});
+}
